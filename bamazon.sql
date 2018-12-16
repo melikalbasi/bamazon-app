88c5392fe38c2b6ec5e1faa7d6f6d96366615a3a
Melika Kalbasi <melikalbasi@gmail.com>
2018-12-15 17:04:20 -0800

    add customer function

--- bamazon.sql
+++ bamazon.sql
@@ -10,17 +10,18 @@ CREATE TABLE products (
   department_name VARCHAR(100),
   price DECIMAL(10,2),
   stock_quantity INT,
+  PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES 
-  ("Dyson Vacuum V10", "Home", 399.99, 100), 
-  ("Nespresso Creatista Plus", "Home", 529.99, 200),
-  ("Vegan Cookbook", "Books", 25, 250),
-  ("Apple AirPods", "Technology", 150, 150),
-  ("Apple Watch", "Technology", 415, 50),
-  ("Heat on Blu-Ray", "Movies", 19, 200),
-  ("Nintendo Switch", "Video Games", 299, 200),
-  ("Super Smash Bros", "Video Games", 59, 170),
-  ("PlayStation 4", "Video Games", 415, 50),
-  ("Echo Dot", "Technology", 40, 900);
+  ("Dyson Vacuum V10", "Home", 399.99, 100000), 
+  ("Nespresso Creatista", "Home", 529.99, 200000),
+  ("Vegan Cookbook", "Books", 25, 250000),
+  ("Apple AirPods", "Technology", 150, 150000),
+  ("Apple Watch", "Technology", 415, 500000),
+  ("Heat on Blu-Ray", "Movies", 19, 200000),
+  ("Nintendo Switch", "Video Games", 299, 200000),
+  ("Super Smash Bros", "Video Games", 59, 1700000),
+  ("PlayStation 4", "Video Games", 415, 5000000),
+  ("Echo Dot", "Technology", 40, 9000000);
---
+++ bamazonCustomer.js
@@ -0,0 +1,129 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+// var Table = require('cli-table');
+require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("======================================================================")
+  console.log("         __")
+  console.log("  _(\    |@@|")
+  console.log(" (__/\__ \--/ __                   Welcome to Bamazon!")
+  console.log("   \___|----|  |   __")
+  console.log("       \ }{ /\ )_ / _\        What would you like to order?")
+  console.log("       /\__/\ \__O (__")
+  console.log("       (--/\--)   \__/               [Quit with Q]")
+  console.log("       _)(  )(_")
+  console.log("      `---''---`")
+  console.log("======================================================================")
+  displayInventory();
+})
+
+
+
+
+// display inventory function
+function displayInventory() {
+  connection.query(
+    "SELECT * FROM products", function (err, res) {
+      if (err) throw err;
+
+      console.table(res);
+
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "itemID",
+            message: "What is the ID of the item you would like to purchase? [Quit with Q]",
+          },
+          {
+            type: "input",
+            name: "itemNum",
+            message: "How many would you like? [Quit with Q]",
+          }
+        ]).then(function(answers) {
+          var userChoice = parseInt(answers.itemID);
+          var itemQuantity = parseInt(answers.itemNum);
+          var currentInventory;
+          var itemPrice;
+
+          for (var i = 0; i < res.length; i++) {
+            if (userChoice === res[i].item_id) {
+              currentInventory = res[i].stock_quantity;
+              itemPrice = res[i].price;
+            }
+          }
+          
+          // check to make sure valid ID was entered
+          if (!currentInventory) {
+            console.log("Sorry you entered an invalid number.")
+            displayInventory();
+          } else {
+            // check to make sure we can handle the quantity they want to order.
+            if (itemQuantity > currentInventory) {
+              console.log("Sorry! This item is sold out.")
+              displayInventory();
+            } else {
+              console.log("Proceeding to update inventory..");
+              updateInventory(userChoice, itemQuantity, currentInventory, itemPrice);
+            }
+          }
+
+
+        })
+    })
+}
+
+
+
+
+function updateInventory (userChoice, itemQuantity, currentInventory, itemPrice) {
+  if (userChoice) {
+    currentInventory -= itemQuantity;
+    connection.query(
+      "UPDATE products SET ? WHERE ?",
+      [
+        {
+          stock_quantity: currentInventory
+        },
+        {
+          item_id: userChoice
+        }
+      ], function(err, res) {
+        if (err) throw err;
+        console.log("Processing your order... ");
+        // console.log(res.affectedRows);
+        displayTotalCost(userChoice, itemQuantity, itemPrice);
+      }
+    )
+    }
+}
+
+
+function displayTotalCost (userChoice, itemQuantity, itemPrice) {
+  var totalCost;
+  totalCost = itemPrice * itemQuantity;
+  console.log(totalCost.toFixed(2));
+
+  inquirer
+    .prompt([
+      {
+
+      }
+    ])
+}
